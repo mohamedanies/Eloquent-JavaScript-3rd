b7f68c9b53334f3a4d8113e006a6ab451fb32ad4
mohamedanies <modysalem22@yahoo.com>
2020-07-06 20:50:54 +0200

    FizzBuzz exersise

---
+++ loops-exercises/FizzBuzz.js
@@ -0,0 +1,17 @@
+for(let num = 0; num < 100; num+=1){
+    if((num % 5 == 0) && (num % 3 == 0)){
+        console.log("FizzBuzz");
+    }
+    else if((num % 3== 0) && (num % 5 != 0)){
+        console.log("Fizz");
+    }
+    else if((num % 5 == 0) && (num % 3 != 0)){
+        console.log("Buzz");
+    }
+    else{
+        console.log(`the number is ${ num }`);
+    }
+
+
+
+}
